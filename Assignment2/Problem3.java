d8db02be2b3e062b80dfc104a74aea1369b6521b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:50:59 +0600

    Update Problem3.java

--- Assignment2/Problem3.java
+++ Assignment2/Problem3.java
@@ -1,1 +1,31 @@
+/*Suppose a class 'A' has a static method to print "Parent". Its subclass 'B' also
+has a static method with the same name to print "Child". Now, call this method by the
+objects of the two classes. Also, call this method by an object of the parent class
+referring to the child class i.e. A obj = new B().*/
+package assignment2.newpackage2;
+public class A {
+    static void display(){
+        System.out.println("Parent");
+    }
+}
+class B extends A{
+    static void display2(){
+        System.out.println("Child");
+    }
+}
+//Main class
 
+
+package assignment2.newpackage2;
+
+public class main {
+    public static void main(String[] args) {
+        A.display();
+        B.display2();
+        
+        A ob= new B();
+        ob.display();
+        
+    }
+ 
+}
