f82b2d9f3f53ed2d68e0ba6e038a860461fcf13b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:00:09 +0600

    Update problem5.java

--- Assignment2/problem5.java
+++ Assignment2/problem5.java
@@ -2,3 +2,31 @@
 value of 'i'. Its subclass also has an integer data member 'j' and a method named
 'printNum' to print the value of 'j'. Make an object of the subclass and use it to
 assign a value to 'i' and to 'j'. Now call the method 'printNum' by this object.*/
+
+package assignment2br;
+public class A {
+    int i;
+    void printNum(){
+        System.out.println("i:"+i);
+    }
+    
+}
+class B extends  A{
+    int j;
+    void printNum(){
+        System.out.println("j:"+j);
+    }
+}
+//Main class
+
+package assignment2br;
+
+public class test {
+    public static void main(String[] args) {
+        B ob= new B();
+        ob.j=3;
+        ob.i=10;
+        ob.printNum();
+    }
+}
+
