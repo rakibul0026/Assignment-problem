b759802817368464db677508c849ddf4cb4d88f6
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:30:24 +0600

    Update problem5.java

--- Assignment3/problem5.java
+++ Assignment3/problem5.java
@@ -1,1 +1,6 @@
-
+/*5) The parent class contains the general information about an account and an abstract
+method to calculate the yearly interest. For savings account, the interest rate is
+10% and for current account the interest rate is 6%. All the data members of the
+Account class are initialized through a parameterized constructor. Your program
+should be able to deposit and withdraw money from a saving account. Perform the
+same operation on a current account.*/
