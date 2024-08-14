2b82d21c87f5da21c8031005a8825eb62920e58d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:26:22 +0600

    Update Problem3.java

--- Assignment2/Problem3.java
+++ Assignment2/Problem3.java
@@ -1,1 +1,30 @@
+package assignment3;
 
+public  abstract class Vehicle {
+	public  abstract void startEngine();
+	public  abstract void stopEngine();
+	
+
+}
+class Car extends Vehicle {
+	public void startEngine()
+	{
+		System.out.println(" Engine start ");
+	}
+	public void stopEngine()
+	{
+		System.out.println(" Engine stop");
+	}
+	
+}
+class Motorcycle extends Vehicle  {
+	
+	public void startEngine()
+	{
+		System.out.println(" Engine start ");
+	}
+	public void stopEngine()
+	{
+		System.out.println(" Engine stop");
+	}
+}
