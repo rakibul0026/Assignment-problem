5d266dcf2c02de75afa513550e08e927238ec316
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:09:18 +0600

    Update Problem3.java

--- Assignment3/Problem3.java
+++ Assignment3/Problem3.java
@@ -1,4 +1,7 @@
-
+/*3) Write a Java program to create an abstract class Vehicle with abstract methods
+startEngine() and stopEngine(). Create subclasses Car and Motorcycle that extend
+the Vehicle class and implement the respective methods to start and stop the
+engines for each vehicle type.*/
 package assignment3;
 
 public  abstract class Vehicle {
