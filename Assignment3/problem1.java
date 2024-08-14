d1f4e28478a08e8eac08cdee86bb8f7a47420094
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:19:14 +0600

    Update problem1.java

--- Assignment3/problem1.java
+++ Assignment3/problem1.java
@@ -3,3 +3,31 @@ class should contain a parameterized constructor to initialize its data member a
 one method to display the information. Now write a Java program that will use an
 array of Student objects to represent information about 3 students. Your program
 should take input from the keyboard and display the information of the 3 students.*/
+package assignment3;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student[] ob = new Student[3];
+        
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter Student ID: ");
+            int Id = sc.nextInt();
+
+            System.out.print("Enter Score: ");
+            int score = sc.nextInt();
+
+            ob[i] = new Student(Id, score);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            ob[i].display();
+        }
+
+        
+    }
+}
+//Main Class
