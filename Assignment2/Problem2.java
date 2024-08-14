c5e9e68ba4567f373a0efa35e37765438f04d6cd
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:43:22 +0600

    Update Problem2.java

--- Assignment2/Problem2.java
+++ Assignment2/Problem2.java
@@ -1,1 +1,36 @@
+package assignment2.newpackage;
+public class Member {
+String  Name;
+int Age;
+String Phonenumber;
+String Address;
+String Salary;
+void print(){
+    System.out.println("name is :"+Name);
+    System.out.println("Age is: "+Age);
+    System.out.println("Phonenumber is: "+Phonenumber);
+    System.out.println("Address is :"+Address);
+     System.out.println("Salary is :"+Salary);
+}
+    void printSalary(){
+    System.out.println("Salary is :"+Salary);
 
+    
+}
+}
+class Employee extends Member{
+    String specialization ;
+    void display1(){
+        print();
+        System.out.println("specialization is:"+specialization);
+        printSalary ();     
+    }  
+}
+class Manager extends Member{
+    String department;
+    void display2(){
+           print();
+        System.out.println("department is" +department);
+         printSalary(); 
+    }
+}
