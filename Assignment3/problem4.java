a506dbf7b6e8bc64eca56fe9893e12966150d5e4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:06:47 +0600

    Update problem4.java

--- Assignment3/problem4.java
+++ Assignment3/problem4.java
@@ -1,1 +1,11 @@
-
+/*4) Implement an abstract class player and two subclasses named batsman and bowler.
+Each player has a name, contact address, telephone number and status (either
+batsman or bowler). The batsman class maintains the total run obtained by a
+batsman and the number of one day matches he participated. Similarly, the bowler
+class maintains the total wickets taken by a player and the total number of matches.
+The parent class contains an abstract method to calculate the average of each
+player. Implement the above classes in Java. Provide constructors to initialize the
+private data. Override the toString() method in each class to display the class name.
+Write a program to create an object of type batsman and bowler and calculate the
+average run/ wickets obtained by a player. Your program should also call the
+toString() method to display the class name*/
