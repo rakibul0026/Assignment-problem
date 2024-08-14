e234b1ba260768927461fc9c3b84a4dc2e5d4ddd
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:57:34 +0600

    Update problem5.java

--- Assignment2/problem5.java
+++ Assignment2/problem5.java
@@ -1,1 +1,4 @@
-
+/*5) A class has an integer data member 'i' and a method named 'printNum' to print the
+value of 'i'. Its subclass also has an integer data member 'j' and a method named
+'printNum' to print the value of 'j'. Make an object of the subclass and use it to
+assign a value to 'i' and to 'j'. Now call the method 'printNum' by this object.*/
