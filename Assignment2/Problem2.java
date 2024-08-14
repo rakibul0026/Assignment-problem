fcc151e9c80403e707762029f4efa94746e466b2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:54:08 +0600

    Update Problem2.java

--- Assignment2/Problem2.java
+++ Assignment2/Problem2.java
@@ -1,3 +1,16 @@
+/*2) Create a class named 'Member' having the following members:
+Data members
+1 - Name
+2 - Age
+3 - Phone number
+4 - Address
+5 - Salary
+It also has a method named ‘printSalary’, which prints the members’ salaries.
+Two classes, ‘Employee’ and ‘Manager’, inherit the 'Member' class. The
+'Employee' and 'Manager' classes have data members 'specialization' and
+'department', respectively. Now, assign a name, age, phone number, address, and
+salary to an employee and a manager by making an object of both of these classes
+and printing the same.*/
 package assignment2.newpackage;
 public class Member {
 String  Name;
