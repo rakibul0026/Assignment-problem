35d292289cf2a428b05bc12dd625afa64ab60b50
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:05:12 +0600

    Update problem1.java

--- Assignment3/problem1.java
+++ Assignment3/problem1.java
@@ -1,1 +1,5 @@
-
+/*1) Design a class named Student that has two private data – student ID and score. The
+class should contain a parameterized constructor to initialize its data member and
+one method to display the information. Now write a Java program that will use an
+array of Student objects to represent information about 3 students. Your program
+should take input from the keyboard and display the information of the 3 students.*/
