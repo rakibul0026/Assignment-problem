b056192038e71f26c1a5ab9768e3c4a5b87ca90a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:24:47 +0600

    Update problem4.java

--- Assignment3/problem4.java
+++ Assignment3/problem4.java
@@ -9,3 +9,111 @@ private data. Override the toString() method in each class to display the class 
 Write a program to create an object of type batsman and bowler and calculate the
 average run/ wickets obtained by a player. Your program should also call the
 toString() method to display the class name*/
+
+// batsman Class
+
+package assignment4;
+
+class batsman extends player {
+    int totalrun;
+    int totalmatches;
+
+ batsman(String name, String contactaddress, String telephonenumber, String status, int totalrun, int totalmatches) {
+        super(name, contactaddress, telephonenumber, status);
+        this.totalrun = totalrun;
+        this.totalmatches = totalmatches;
+    }
+  void average() {
+        int av1 = totalrun / totalmatches;
+        System.out.println("Total run is :" +totalrun);
+        System.out.println("totalmatches is :"+totalmatches);
+        System.out.println("average run  is:" +av1);
+     
+    }
+    public String toString() {
+        return "Batsman: " + name;
+        
+    }
+}
+
+//bowler class
+
+package assignment4;
+
+class bowler extends player {
+    int  totalwickets;
+    int totalmatches;
+
+ bowler(String name, String contactaddress, String telephonenumber, String status, int totalwickets, int totalmatches) {
+        super(name, contactaddress, telephonenumber, status);
+        this.totalwickets = totalwickets;
+        this.totalmatches = totalmatches;
+    }
+
+ void average() {
+        float av2 = totalwickets / totalmatches;
+        System.out.println("totalwickets is :"+totalwickets);
+        System.out.println("totalmatches is :"+totalmatches);
+        System.out.println("average wickets  is:" +av2);
+     
+    }
+ public String toString() {
+        return "Bowler: " + name;
+        
+    }
+}
+
+//Player class
+
+package assignment4;
+
+public abstract class player {
+    String name;
+    String contactaddress;
+    String telephonenumber;
+    String status;
+
+    player(String name, String contactaddress, String telephonenumber, String status) {
+        this.name = name;
+        this.contactaddress = contactaddress;
+        this.telephonenumber = telephonenumber;
+        this.status = status;
+    }
+
+     abstract void average() ;
+        
+   void display() {
+        System.out.println("name is :" + name);
+        System.out.println("contactaddress is :" + contactaddress);
+        System.out.println("telephonenumber is :" + telephonenumber);
+        System.out.println("status is :" + status);
+       
+    }
+}
+
+//Main class
+
+package assignment4;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+bowler ob = new bowler("masrafee" , "Narail " ,"019*****","bowler",250,150);
+System.out.println("The class name is : "+ob.toString());
+ob.display();
+ob.average();
+
+
+batsman ob1 = new batsman("tamim" , "chattogram " ,"017*****","batsman",10000,200);
+System.out.println("The class name is : "+ob1.toString());
+ob1.display();
+ob1.average();
+ob1.toString();
+
+	}
+
+}
+
+
+
