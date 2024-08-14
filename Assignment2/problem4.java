729ae74bbf2a78597e1741906d83cebc981289a0
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:55:27 +0600

    Update problem4.java

--- Assignment2/problem4.java
+++ Assignment2/problem4.java
@@ -1,4 +1,9 @@
-
+/*4) Create a class named 'Shape' with a method to print "This is a shape". Then, create
+two other classes named 'Rectangle' and 'Circle', inheriting the Shape class, both
+having a method to print "This is a rectangular shape" and "This is a circular
+shape," respectively. Create a subclass 'Square' of 'Rectangle' having a method to
+print "Square is a rectangle". Now call the method of the 'Shape' and 'Rectangle'
+class by the object of the 'Square' class.*/
 package assignment2.newpackage1;
 
 public class Shape {
