a6c93943f1585aee2e0cd16f57400dff5763a866
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:08:25 +0600

    Update Problem2.java

--- Assignment3/Problem2.java
+++ Assignment3/Problem2.java
@@ -1,3 +1,7 @@
+/*2) Write a Java program to create an abstract class Animal with abstract methods
+eat() and sleep(). Create subclasses Lion, Tiger, and Deer that extend the Animal
+class and implement the eat() and sleep() methods differently based on their
+specific behavior.*/
 package assignment3;
 
 abstract class Animal {
