893b01da7c304201a7bce7e0a73b2e7bbf225d34
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 01:32:52 +0600

    Update Problem2.java

--- Assignment3/Problem2.java
+++ Assignment3/Problem2.java
@@ -1,1 +1,59 @@
+package assignment3;
+
+abstract class Animal {
+	abstract void eat();
+	abstract void sleep();
+	
+}
+class Lion extends Animal 
+{
+	void eat() {
+		System.out.println(" Lion eats meat");
+	}
+		void sleep() {
+			System.out.println("Lion sleeps in the jungle"+"\n");
+      }
+}
+class Tiger extends Animal 
+{
+	void eat() {
+		System.out.println("Tiger eats meat");
+	}
+		void sleep() {
+			System.out.println("Tiger sleeps in the jungle"+"\n");
+      }
+	
+}
+class Deer extends Animal 
+{
+	void eat() {
+		System.out.println("Deer eats meat");
+	}
+		void sleep() {
+			System.out.println("Deer sleeps in the deep jungle"+"\n");
+      }
+	
+	
+}
+//Main class
+package assignment3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 Animal ob=new  Lion();
+		 ob.eat();
+		 ob.sleep();
+		 
+		 Animal ob1=new  Tiger();
+		 ob1.eat();
+		 ob1.sleep();
+		 
+		 Animal ob2=new Deer();
+		 ob2.eat();
+		 ob2.sleep();
+	}
+
+}
+
 
