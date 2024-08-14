19a74d1f2f94543e2100768434b0ce1a974e9b11
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-15 02:36:44 +0600

    Update lab test problem.java

--- lab test problem.java
+++ lab test problem.java
@@ -1,1 +1,66 @@
+package labtest;
+/*Write a Java program to create a class known as "BankAccount" with methods 
+ called deposit() and withdraw(). Create a subclass called SavingsAccount that
+ overrides the withdraw() method to prevent withdrawals if the account balance
+  falls below one hundred.
+ */
+
+
+class BankAccount {
+    int accountnumber;
+    double accountbalance;
+
+    BankAccount(int accountnumber, double accountbalance) {
+        this.accountbalance = accountbalance;
+        this.accountnumber = accountnumber;
+    }
+
+    void deposit(double amount) {
+        accountbalance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (accountbalance > amount)
+            accountbalance -= amount;
+        else
+            System.out.println("Insufficient balance");
+    }
+}
+
+//savingAccount class
+package labtest;
+
+class savingAccount extends BankAccount {
+    savingAccount(int a, int b) {
+        super(a, b);
+    }
+
+    void withdraw(double amount) {
+        if (accountbalance - amount < 100)
+            System.out.println("Error: Insufficient balance");
+        else
+            super.withdraw(amount); // Call the withdraw method from the superclass to update the balance
+    }
+}
+
+//Main class 
+
+package labtest;
+
+public class classmain {
+    public static void main(String[] args) {
+        savingAccount ac = new savingAccount(1222, 1000);
+        ac.deposit(500);
+        System.out.println("Balance after deposit: $" + ac.accountbalance);
+
+        //ac.deposit(600);
+        //System.out.println("Balance after deposit: $" + ac.accountbalance);
+
+        ac.withdraw(900);
+        System.out.println("Balance after withdrawal: $" + ac.accountbalance);
+        
+        ac.withdraw(200);
+        System.out.println("Balance after withdrawal: $" + ac.accountbalance);
+    }
+}
 
