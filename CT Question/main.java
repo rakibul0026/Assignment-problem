069fca35d40f0ea2049ce1c5b1ba0a7669fbe3cf
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 02:28:27 +0600

    Update main.java

--- CT Question/main.java
+++ CT Question/main.java
@@ -1,1 +1,20 @@
 
+
+
+
+
+package classct;
+
+public class main {
+
+	public static void main(String[] args) {
+		text obj=new text();
+		//obj.age=21;
+		//obj.name="rana";
+		obj. display();
+		
+
+	}
+
+}
+
