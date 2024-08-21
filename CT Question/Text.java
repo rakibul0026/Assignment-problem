7bd858472d8f6a1dcc5747fa8405e9050fbc0b2b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 02:31:02 +0600

    Update Text.java

--- CT Question/Text.java
+++ CT Question/Text.java
@@ -1,1 +1,15 @@
+package classct;
+
+public class text {
+	int age;
+String name;
+  static  void display()
+{
+	//System.out.println(age+" "+ name);
+	//System.out.println(age);
+	//System.out.println(10+20+"java");
+	System.out.println("java"+10*20);
+}
+
+}
 
